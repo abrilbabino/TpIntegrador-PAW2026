cfb863bc6eca0bd493a5fa4c729f6fa2b6af537e
NaiaraCollazo <collazonaiara@gmail.com>
2026-04-28 18:50:47 -0300

    Add ErrorController and views for error handling; update PageController and index view

---
+++ Entrega3/src/App/Controllers/ErrorController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Paw\App\Controllers;
+
+use Paw\Core\Controller;
+
+class ErrorController extends Controller
+{
+
+    public function notFound()
+    {
+        http_response_code(404);
+        $titulo = 'Pagina no encontrada';
+        $menu = $this->menu;
+        $redes = $this->redes;
+        require $this -> viewsDir . '/not-found.view.php';
+    }
+    public function internalError()
+    {
+        http_response_code(500);
+        $titulo = "Error interno del servidor";
+        $menu = $this->menu;
+        $redes = $this->redes;
+        require $this->viewsDir . '/internal_error.view.php';
+    }
+    public function invalidFormat($e){
+        http_response_code(400);
+        $titulo = 'Invalid Format';
+        $menu = $this->menu;
+        $redes = $this->redes;
+        require $this->viewsDir . '/invalid_format.view.php';
+    }
+
+
+}
---
+++ Entrega3/src/App/Views/index.view.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/png" href="/assets/img/icon.png">
+    <link rel="stylesheet" href="/assets/css//style.css" />
+    <link rel="stylesheet" href="/assets/css/print.css" media="print" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+    />
+     <title>PawMap</title>
+</head>
+<body>
+    <header class="hero">
+          <h1>Encuentra a tu nuevo<br>mejor amigo</h1>
+          <p>Explora mascotas cerca de tu ubicación.</p>
+      </header>
+
+
+      <section class="puntos-refugios">
+        <figure>
+          <iframe
+            src="https://www.google.com/maps/d/embed?mid=1fqEzq6nPa6IITzVkvMWnMvfQsy2sqrQ&ehbc=2E312F&noprof=1"
+          ></iframe>
+        </figure>
+      </section>
+
+    <section class="test-compatibilidad">
+        <h2>¡Encontrá tu match!</h2>
+        <p>¿Qué tipo de mascota buscás?</p>
+        <a href="/test-de-compatibilidad" class="btn-realizar-test">Realizar Test</a>
+    </section>
+
+
+    <section class="grilla-mascotas">
+        <h2>Mascotas en Adopción</h2>
+        <a href="/mascotas" class="btn-ver-mascotas">Ver Todas las mascotas</a>
+    </section>
+ 
+</body>
+</html>
