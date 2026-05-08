b2b00b1025fc852a9f9fc3977ac9ad4b77f4cee2
Tobias Avila <tobiasavila04@gmail.com>
2026-05-08 20:03:25 -0300

    arreglo configmap

--- Entrega3/src/Core/Config.php
+++ Entrega3/src/Core/Config.php
@@ -8,8 +8,8 @@ class Config
 
     public function __construct()
     {
-        $this->configs["LOG_LEVEL"] = getenv("LOG_LEVEL", "INFO");
-        $path = getenv("LOG_PATH", "/logs/app.log");
+        $this->configs["LOG_LEVEL"] = getenv("LOG_LEVEL") ?: "INFO";
+        $path = getenv("LOG_PATH") ?: "logs/app.log";
         $this->configs["LOG_PATH"] = $this->joinPaths(__DIR__ . '/../../', $path);
 
         $this->configs["DB_ADAPTER"]  = getenv("DB_ADAPTER")  ?? 'pgsql';
