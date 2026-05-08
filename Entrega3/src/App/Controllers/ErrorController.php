a9b2844af723f79bb3dd45e27b9fcb5fea5e493b
Tobias Avila <tobiasavila04@gmail.com>
2026-05-08 15:10:42 -0300

    Revert ErrorController - discard headers_sent changes

--- Entrega3/src/App/Controllers/ErrorController.php
+++ Entrega3/src/App/Controllers/ErrorController.php
@@ -9,7 +9,7 @@ class ErrorController extends Controller
 
     public function notFound()
     {
-        if (!headers_sent()) http_response_code(404);
+        http_response_code(404);
         $titulo = 'Pagina no encontrada';
         $menu = $this->menu;
         $redes = $this->redes;
@@ -17,14 +17,14 @@ class ErrorController extends Controller
     }
     public function internalError()
     {
-        if (!headers_sent()) http_response_code(500);
+        http_response_code(500);
         $titulo = "Error interno del servidor";
         $menu = $this->menu;
         $redes = $this->redes;
         require $this->viewsDir . '/internal_error.view.php';
     }
     public function invalidFormat($e){
-        if (!headers_sent()) http_response_code(400);
+        http_response_code(400);
         $titulo = 'Invalid Format';
         $menu = $this->menu;
         $redes = $this->redes;
