7381723191353b444b8ec4ddd5e2c9de36224382
abrilbabino <abril.nadia.babino984@gmail.com>
2026-08-04 14:24:58 -0300

    Formateo edad mascota

--- Entrega3/src/App/Helpers/TwigExtensions.php
+++ Entrega3/src/App/Helpers/TwigExtensions.php
@@ -4,36 +4,19 @@ namespace Paw\App\Helpers;
 
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Paw\App\Helpers\DateHelper;
 
 class TwigExtensions extends AbstractExtension
 {
     public function getFilters()
     {
         return [
-            new TwigFilter('edad_formateada', [$this, 'formatEdad']),
+            new TwigFilter('edad_formateada', [self::class, 'formatEdad']),
         ];
     }
 
-    public function formatEdad($fechaNac, $edadFallback = 0)
+    public static function formatEdad($fechaNac, $edadFallback = 0)
     {
-        if (empty($fechaNac)) {
-            $edadFallback = $edadFallback ?? 0;
-            return $edadFallback . ($edadFallback == 1 ? ' año' : ' años');
-        }
-        try {
-            $d = new \DateTime($fechaNac);
-            $hoy = new \DateTime();
-            $diff = $hoy->diff($d);
-            if ($diff->y >= 1) {
-                return $diff->y . ($diff->y == 1 ? ' año' : ' años');
-            } elseif ($diff->m >= 1) {
-                return $diff->m . ($diff->m == 1 ? ' mes' : ' meses');
-            } else {
-                return $diff->d . ($diff->d == 1 ? ' día' : ' días');
-            }
-        } catch (\Exception $e) {
-            $edadFallback = $edadFallback ?? 0;
-            return $edadFallback . ($edadFallback == 1 ? ' año' : ' años');
-        }
+        return DateHelper::formatEdad($fechaNac, $edadFallback);
     }
 }
